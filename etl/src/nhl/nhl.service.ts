94eeef7ea95837114a1abb9669482eb6482326c5
Jelani Jackson <jelani.t.jackson@gmail.com>
2023-05-31 17:58:14 -0500

    Added mock responses to nhl service

--- etl/src/nhl/nhl.service.ts
+++ etl/src/nhl/nhl.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class NhlService {}
+export class NhlService {
+  async getSeason(seasonId: string) {
+    return Promise.resolve({ seasonId, status: 'Season Retrieved' });
+  }
+
+  async getGame(gameId: string) {
+    return Promise.resolve({ gameId, status: 'Game Retrieved' });
+  }
+}
