8a18d51dbc588ad0e194491c3d88d19cef01ab56
Jelani Jackson <jelani.t.jackson@gmail.com>
2023-05-31 18:41:21 -0500

    Fleshed out NhlService

--- etl/src/nhl/nhl.service.ts
+++ etl/src/nhl/nhl.service.ts
@@ -1,12 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { Axios } from 'axios';
+import { Game } from 'src/game/entities/game.entity';
+import { Season } from 'src/season/entities/season.entity';
+
+const nhlApiBaseUrl = 'https://statsapi.web.nhl.com/api/v1';
 
 @Injectable()
 export class NhlService {
-  async getSeason(seasonId: string) {
-    return Promise.resolve({ seasonId, status: 'Season Retrieved' });
+  private readonly axios: Axios;
+
+  constructor() {
+    this.axios = new Axios({
+      baseURL: nhlApiBaseUrl,
+      headers: { 'Content-Type': 'application/json' },
+    });
   }
 
-  async getGame(gameId: string) {
-    return Promise.resolve({ gameId, status: 'Game Retrieved' });
+  async getSeason(seasonId: string): Promise<Season> {
+    const { data: seasonResponse } = await this.axios.get(
+      `/schedule?season=${seasonId}`,
+    );
+
+    return JSON.parse(seasonResponse);
+  }
+
+  async getGame(gameId: string): Promise<Game> {
+    const { data: gameResponse } = await this.axios.get(
+      `/game/${gameId}/feed/live`,
+    );
+
+    return JSON.parse(gameResponse);
   }
 }
