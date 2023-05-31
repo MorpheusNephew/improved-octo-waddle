ad0009dc4cad99f5d9c101b079338380e9e4782b
Jelani Jackson <jelani.t.jackson@gmail.com>
2023-05-31 17:43:15 -0500

    Removed excess for resource generation

---
+++ etl/src/game/dto/load-game.dto.ts
@@ -0,0 +1,3 @@
+export class LoadGameDto {
+  gameId: string;
+}
--- etl/src/game/game.service.ts
+++ etl/src/game/game.service.ts
@@ -1,26 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { CreateGameDto } from './dto/create-game.dto';
-import { UpdateGameDto } from './dto/update-game.dto';
+import { LoadGameDto } from './dto/load-game.dto';
 
 @Injectable()
 export class GameService {
-  create(createGameDto: CreateGameDto) {
-    return 'This action adds a new game';
-  }
-
-  findAll() {
-    return `This action returns all game`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} game`;
-  }
-
-  update(id: number, updateGameDto: UpdateGameDto) {
-    return `This action updates a #${id} game`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} game`;
+  load({ gameId }: LoadGameDto) {
+    return `This action loads a new game from gameId: ${gameId}`;
   }
 }
