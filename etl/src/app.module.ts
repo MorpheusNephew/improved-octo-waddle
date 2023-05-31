2e61bedf3abd4f699162319ffc0b7654f543e7f3
Jelani Jackson <jelani.t.jackson@gmail.com>
2023-05-31 17:33:22 -0500

    Generated game resource

--- etl/src/app.module.ts
+++ etl/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SeasonModule } from './season/season.module';
+import { GameModule } from './game/game.module';
 
 @Module({
-  imports: [SeasonModule],
+  imports: [SeasonModule, GameModule],
   controllers: [AppController],
   providers: [AppService],
 })
