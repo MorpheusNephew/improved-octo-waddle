bc971a50af5e2251926740a2fa50e62c2dfa2b61
Jelani Jackson <jelani.t.jackson@gmail.com>
2023-05-31 17:39:02 -0500

    Removed excess from resource generation

---
+++ etl/src/season/dto/load-season.ts
@@ -0,0 +1,3 @@
+export class LoadSeasonDto {
+  seasonId: string;
+}
--- etl/src/season/season.service.ts
+++ etl/src/season/season.service.ts
@@ -1,26 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { CreateSeasonDto } from './dto/create-season.dto';
-import { UpdateSeasonDto } from './dto/update-season.dto';
+import { LoadSeasonDto } from './dto/load-season';
 
 @Injectable()
 export class SeasonService {
-  create(createSeasonDto: CreateSeasonDto) {
-    return 'This action adds a new season';
-  }
-
-  findAll() {
-    return `This action returns all season`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} season`;
-  }
-
-  update(id: number, updateSeasonDto: UpdateSeasonDto) {
-    return `This action updates a #${id} season`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} season`;
+  load({ seasonId }: LoadSeasonDto) {
+    return `This action loads a new season from seasonId: ${seasonId}`;
   }
 }
