32b4f2fe8eb3152b4c28caed493bd5605b468140
Jelani Jackson <jelani.t.jackson@gmail.com>
2023-05-31 17:49:15 -0500

    Adding NHL service to make calls for a season/game

--- etl/src/app.module.ts
+++ etl/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SeasonModule } from './season/season.module';
 import { GameModule } from './game/game.module';
+import { NhlService } from './nhl/nhl.service';
 
 @Module({
   imports: [SeasonModule, GameModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, NhlService],
 })
 export class AppModule {}
