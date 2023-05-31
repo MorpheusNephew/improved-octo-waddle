1ab87906b52bce5b80ec70b0c55a19703879e6a8
Jelani Jackson <jelani.t.jackson@gmail.com>
2023-05-31 18:40:45 -0500

    WIP: Updated season entity

--- etl/src/season/entities/season.entity.ts
+++ etl/src/season/entities/season.entity.ts
@@ -1,1 +1,13 @@
-export class Season {}
+export class Season {
+  copyright: string;
+  totalItems: number;
+  metaData: {
+    timeStamp: string;
+  };
+  dates: {
+    date: string;
+    games: {
+      gamePk: number;
+    }[];
+  }[];
+}
