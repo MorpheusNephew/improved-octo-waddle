95e5766fbc16ca86f56ce68a2d996a031108ea18
Jelani Jackson <jelani.t.jackson@gmail.com>
2023-06-02 06:08:25 -0500

    Fleshed out GameDto

--- etl/src/nhl/dto/game.dto.ts
+++ etl/src/nhl/dto/game.dto.ts
@@ -1,1 +1,83 @@
-export class GameDto {}
+export class TeamDto {
+  id: number;
+  name: string;
+}
+
+export class PositionDto {
+  code: string;
+  name: string;
+  type: string;
+  abbreviation: string;
+}
+
+export class PlayerDto {
+  id: number;
+  fullName: string;
+  firstName: string;
+  lastName: string;
+  primaryNumber: string;
+  birthDate: string;
+  currentAge: number;
+  currentTeam: TeamDto;
+  primaryPosition: PositionDto;
+}
+
+export class StatsDto {
+  assists: number;
+  goals: number;
+  hits?: number;
+  penaltyMinutes?: number;
+}
+
+export class GoalieStatsDto extends StatsDto {}
+
+export class SkaterStatsDto extends StatsDto {
+  hits: number;
+  penaltyMinutes: number;
+}
+
+export class PlayerStatsDto {
+  goalieStats?: GoalieStatsDto;
+  skaterStats?: SkaterStatsDto;
+}
+
+export class PlayerGameDto {
+  person: Pick<PlayerDto, 'id' | 'fullName'>;
+  jerseyNumber: string;
+  position: PositionDto;
+  stats: PlayerStatsDto;
+}
+
+export class GameDto {
+  gamePk: number;
+  gameData: {
+    game: {
+      pk: number;
+      season: string;
+      type: string;
+    };
+    datetime: {
+      dateTime: string;
+    };
+    status: {
+      abstractGameState: string;
+    };
+    teams: {
+      home: TeamDto;
+      away: TeamDto;
+    };
+    players: Record<string, PlayerDto>;
+  };
+  liveData: {
+    boxscore: {
+      teams: {
+        home: {
+          players: Record<string, PlayerGameDto>;
+        };
+        away: {
+          players: Record<string, PlayerGameDto>;
+        };
+      };
+    };
+  };
+}
