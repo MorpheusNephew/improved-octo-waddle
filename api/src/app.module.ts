13f5740c4bf51c0622719e87896db08f9a70e51d
Jelani Jackson <jelani.t.jackson@gmail.com>
2023-06-03 10:17:25 -0500

    Initializing sequelize for postgres

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,14 +4,29 @@ import { AppService } from './app.service';
 import { PlayerStatsModule } from './player-stats/player-stats.module';
 import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: ['.env'] }),
     PlayerStatsModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       playground: true,
       autoSchemaFile: 'schema.gql',
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: process.env.POSTGRES_PORT
+        ? parseInt(process.env.POSTGRES_PORT)
+        : 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      autoLoadModels: true,
+      synchronize: true,
     }),
   ],
   controllers: [AppController],
