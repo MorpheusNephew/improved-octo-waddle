26a0ff409d1a2e625fc8f582a74481b1812efa86
Jelani Jackson <jelani.t.jackson@gmail.com>
2023-06-03 10:17:42 -0500

    Added PlayerGameStat model

---
+++ api/src/player-stats/models/playerGameStat.model.ts
@@ -0,0 +1,51 @@
+import { Column, Model, PrimaryKey, Table } from 'sequelize-typescript';
+
+@Table
+export class PlayerGameStat extends Model {
+  @PrimaryKey
+  @Column
+  gameId: string;
+
+  @PrimaryKey
+  @Column
+  playerId: number;
+
+  @Column
+  playerName: string;
+
+  @Column
+  teamId: number;
+
+  @Column
+  teamName: string;
+
+  @Column
+  playerAge: number;
+
+  @Column
+  playerNumber: string;
+
+  @Column
+  playerPosition: string;
+
+  @Column
+  assists: number;
+
+  @Column
+  goals: number;
+
+  @Column({ defaultValue: 0 })
+  hits: number;
+
+  @Column
+  points: number;
+
+  @Column({ defaultValue: 0 })
+  penaltyMinutes: number;
+
+  @Column
+  opponentTeamName: string;
+
+  @Column
+  opponentTeamId: number;
+}
