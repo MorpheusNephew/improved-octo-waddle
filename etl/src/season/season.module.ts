8e33013e9941909bca682ca042af1a395b43a02c
Jelani Jackson <jelani.t.jackson@gmail.com>
2023-06-03 09:36:58 -0500

    Using game service inside of season service to store game stats for an entire season (WIP)

--- etl/src/season/season.module.ts
+++ etl/src/season/season.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { SeasonService } from './season.service';
 import { SeasonController } from './season.controller';
 import { NhlService } from 'src/nhl/nhl.service';
+import { GameService } from 'src/game/game.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { PlayerGameStat } from 'src/game/models/playerGameStat.model';
 
 @Module({
+  imports: [SequelizeModule.forFeature([PlayerGameStat])],
   controllers: [SeasonController],
-  providers: [SeasonService, NhlService],
+  providers: [SeasonService, NhlService, GameService],
 })
 export class SeasonModule {}
--- etl/src/season/season.service.ts
+++ etl/src/season/season.service.ts
@@ -1,12 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { LoadSeasonDto } from './dto/load-season.dto';
 import { NhlService } from 'src/nhl/nhl.service';
+import { GameService } from 'src/game/game.service';
 
 @Injectable()
 export class SeasonService {
-  constructor(private readonly nhlService: NhlService) {}
+  constructor(
+    private readonly nhlService: NhlService,
+    private readonly gameService: GameService,
+  ) {}
 
   async load({ seasonId }: LoadSeasonDto) {
-    return this.nhlService.getSeason(seasonId);
+    const season = await this.nhlService.getSeason(seasonId);
+
+    await Promise.all(
+      season.dates.map(
+        async (date) =>
+          await Promise.all(
+            date.games.map((game) =>
+              this.gameService.load({ gameId: game.gamePk }),
+            ),
+          ),
+      ),
+    );
+
+    return season;
   }
 }
