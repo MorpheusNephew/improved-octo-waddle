36794c6ff34b31de125dedb74436d9d32622212f
Jelani Jackson <jelani.t.jackson@gmail.com>
2023-06-03 11:20:10 -0500

    Fleshed out PlayerStats

--- api/src/player-stats/entities/player-stat.entity.ts
+++ api/src/player-stats/entities/player-stat.entity.ts
@@ -1,7 +1,49 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
 
 @ObjectType()
-export class PlayerStat {
-  @Field(() => String, { description: 'Id of the game' })
-  gameId: string;
+export class PlayerStats {
+  @Field(() => Int, { description: 'Id of the game' })
+  gameId: number;
+
+  @Field(() => Int, { description: '' })
+  playerId: number;
+
+  @Field(() => String, { description: '' })
+  playerName: string;
+
+  @Field(() => Int, { description: '' })
+  teamId: number;
+
+  @Field(() => String, { description: '' })
+  teamName: string;
+
+  @Field(() => Int, { description: '' })
+  playerAge: number;
+
+  @Field(() => String, { description: '' })
+  playerNumber: string;
+
+  @Field(() => String, { description: '' })
+  playerPosition: string;
+
+  @Field(() => Int, { description: '' })
+  assists: number;
+
+  @Field(() => Int, { description: '' })
+  goals: number;
+
+  @Field(() => Int, { description: '' })
+  hits: number;
+
+  @Field(() => Int, { description: '' })
+  points: number;
+
+  @Field(() => Int, { description: '' })
+  penaltyMinutes: number;
+
+  @Field(() => String, { description: '' })
+  opponentTeamName: string;
+
+  @Field(() => Int, { description: '' })
+  opponentTeamId: number;
 }
