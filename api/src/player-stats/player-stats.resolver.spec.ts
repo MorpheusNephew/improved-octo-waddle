fd16bf08e96326f6e01312598dd3e0ece92d4f7e
Jelani Jackson <jelani.t.jackson@gmail.com>
2023-06-04 12:29:55 -0500

    Added unit tests for loading players stats

--- api/src/player-stats/player-stats.resolver.spec.ts
+++ api/src/player-stats/player-stats.resolver.spec.ts
@@ -1,13 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PlayerStatsResolver } from './player-stats.resolver';
 import { PlayerStatsService } from './player-stats.service';
+import { randomUUID } from 'crypto';
 
 describe('PlayerStatsResolver', () => {
   let resolver: PlayerStatsResolver;
+  let mockedLoadPlayersStats = jest.fn();
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PlayerStatsResolver, PlayerStatsService],
+      providers: [
+        PlayerStatsResolver,
+        {
+          provide: PlayerStatsService,
+          useValue: { loadPlayersStats: mockedLoadPlayersStats },
+        },
+      ],
     }).compile();
 
     resolver = module.get<PlayerStatsResolver>(PlayerStatsResolver);
@@ -16,4 +24,25 @@ describe('PlayerStatsResolver', () => {
   it('should be defined', () => {
     expect(resolver).toBeDefined();
   });
+
+  describe('loadPlayersStats', () => {
+    it.each([{ gameId: 200928333 }, { seasonId: '20082009' }])(
+      'should receive a response with statusId',
+      ({ gameId, seasonId }) => {
+        // Arrange
+        const statusId = randomUUID();
+        const expectedResult = { statusId };
+
+        mockedLoadPlayersStats.mockReturnValueOnce(statusId);
+
+        // Act
+        const result = resolver.loadPlayersStats({ gameId, seasonId });
+
+        // Assert
+        expect(result).toStrictEqual(expectedResult);
+      },
+    );
+  });
+
+  describe('queryPlayersStats', () => {});
 });
