cfa31a9ca20e47ad4576d1098fa894414acb5a0d
Jelani Jackson <jelani.t.jackson@gmail.com>
2023-06-03 23:13:21 -0500

    Configured rabbitmq client

--- api/src/player-stats/player-stats.module.ts
+++ api/src/player-stats/player-stats.module.ts
@@ -3,9 +3,27 @@ import { PlayerStatsService } from './player-stats.service';
 import { PlayerStatsResolver } from './player-stats.resolver';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { PlayerGameStat } from './models/playerGameStat.model';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [SequelizeModule.forFeature([PlayerGameStat])],
+  imports: [
+    SequelizeModule.forFeature([PlayerGameStat]),
+    ClientsModule.register([
+      {
+        name: 'INGEST_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: [
+            `amqp://${process.env.RABBITMQ_HOST}:${process.env.RABBITMQ_PORT}`,
+          ],
+          queue: process.env.RABBITMQ_QUEUE,
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+  ],
   providers: [PlayerStatsResolver, PlayerStatsService],
 })
 export class PlayerStatsModule {}
