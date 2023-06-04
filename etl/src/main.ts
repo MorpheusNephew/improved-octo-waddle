6769e9bddbbc02fdf5c8cbb09b6f3b7845e76a21
Jelani Jackson <jelani.t.jackson@gmail.com>
2023-06-03 22:41:34 -0500

    Converted etl app into a microservice

--- etl/src/main.ts
+++ etl/src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
-const PORT = process.env.PORT ?? 3000;
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(PORT);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [
+          `amqp://${process.env.RABBITMQ_HOST}:${process.env.RABBITMQ_PORT}`,
+        ],
+        queue: process.env.RABBITMQ_QUEUE,
+        queueOptions: {
+          durable: false,
+        },
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
