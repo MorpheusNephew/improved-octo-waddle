bf7f7577b9cdb81b97706a2dcd802f0b6cd1b4dd
Jelani Jackson <jelani.t.jackson@gmail.com>
2023-06-03 22:52:36 -0500

    Added logging and try-catches for debugging

--- etl/src/game/game.service.ts
+++ etl/src/game/game.service.ts
@@ -1,20 +1,37 @@
 import { Injectable } from '@nestjs/common';
-import { LoadGameDto } from './dto/load-game.dto';
 import { NhlService } from 'src/nhl/nhl.service';
 import { isEmpty } from 'lodash';
 import { PlayerGameDto, TeamDto } from 'src/nhl/dto/game.dto';
 import { PlayerGameStat } from './models/playerGameStat.model';
 import { InjectModel } from '@nestjs/sequelize';
+import { Logger, createLogger, transports } from 'winston';
 
 @Injectable()
 export class GameService {
+  private readonly logger: Logger;
+
   constructor(
     private readonly nhlService: NhlService,
     @InjectModel(PlayerGameStat)
     private readonly playerGameStatModel: typeof PlayerGameStat,
-  ) {}
+  ) {
+    this.logger = createLogger({
+      transports: [
+        new transports.File({
+          level: 'error',
+          handleRejections: true,
+          handleExceptions: true,
+          filename: `logs/errors/${new Date().toISOString()}-etl.log`,
+        }),
+        new transports.File({
+          level: 'info',
+          filename: `logs/info/${new Date().toISOString()}-etl.log`,
+        }),
+      ],
+    });
+  }
 
-  async load({ gameId }: LoadGameDto) {
+  async load(gameId: number) {
     const game = await this.nhlService.getGame(gameId);
 
     const {
@@ -34,9 +51,11 @@ export class GameService {
 
     const extractPlayersStats = (
       playersInfo: Record<string, PlayerGameDto>,
+      team: TeamDto,
       opponentTeam: TeamDto,
     ) =>
       Object.entries(playersInfo)
+        .filter((playerInfo) => !isEmpty(playerInfo))
         .filter(([_id, player]) => !isEmpty(player.stats))
         .map(([id, player]) => {
           const playerInfo = allPlayers[id];
@@ -47,11 +66,11 @@ export class GameService {
             gameId,
             playerId: player.person.id,
             playerName: player.person.fullName,
-            teamId: playerInfo.currentTeam.id,
-            teamName: playerInfo.currentTeam.name,
+            teamId: team.id,
+            teamName: team.name,
             playerAge: playerInfo.currentAge,
             playerNumber: playerInfo.primaryNumber,
-            playerPosition: playerInfo.primaryPosition.name,
+            playerPosition: player.position.name,
             assists: playerStats.assists,
             goals: playerStats.goals,
             hits: playerStats.hits,
@@ -62,17 +81,45 @@ export class GameService {
           };
         });
 
-    const homePlayerStats = extractPlayersStats(homePlayers, awayTeam);
-    const awayPlayerStats = extractPlayersStats(awayPlayers, homeTeam);
+    if (isEmpty(allPlayers)) {
+      return game;
+    }
 
-    const allPlayerStats = [...homePlayerStats, ...awayPlayerStats];
+    try {
+      const homePlayerStats = extractPlayersStats(
+        homePlayers,
+        homeTeam,
+        awayTeam,
+      );
+      const awayPlayerStats = extractPlayersStats(
+        awayPlayers,
+        awayTeam,
+        homeTeam,
+      );
 
-    const results = await this.playerGameStatModel.bulkCreate(allPlayerStats, {
-      ignoreDuplicates: true,
-    });
+      const allPlayerStats = [...homePlayerStats, ...awayPlayerStats];
 
-    console.log(results);
+      this.logger.info({ allPlayerStats, homePlayerStats, awayPlayerStats });
 
-    return game;
+      const createResult = await this.playerGameStatModel.bulkCreate(
+        allPlayerStats,
+        {
+          ignoreDuplicates: true,
+        },
+      );
+
+      this.logger.info({ createResult });
+
+      return game;
+    } catch (currentError) {
+      this.logger.error({
+        currentError,
+        gameId,
+        homePlayers,
+        homeTeam,
+        awayPlayers,
+        awayTeam,
+      });
+    }
   }
 }
