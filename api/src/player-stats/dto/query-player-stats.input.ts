2816c6dca82653b92a7b7652039e5bd9fd4e9004
Jelani Jackson <jelani.t.jackson@gmail.com>
2023-06-04 15:24:33 -0500

    Documented properties

--- api/src/player-stats/dto/query-player-stats.input.ts
+++ api/src/player-stats/dto/query-player-stats.input.ts
@@ -1,61 +1,77 @@
 import { InputType, Field, Int } from '@nestjs/graphql';
 
 @InputType()
-export class QueryPlayerStatsQueryInput {}
-
-@InputType()
 export class QueryPlayerStatsQueryOptionsInput {
-  @Field(() => Int, { description: '', nullable: true })
+  @Field(() => Int, { nullable: true })
   limit?: number;
 
-  @Field(() => Int, { description: '', nullable: true })
+  @Field(() => Int, { nullable: true })
   offset?: number;
 }
 
 @InputType()
 class QueryPlayerStatsStatisticsInput {
-  @Field(() => String, { description: '' })
+  @Field(() => String, {
+    description: 'The operator to use in comparison to a value',
+  })
   operator: 'eq' | 'lt' | 'lte' | 'gt' | 'gte';
 
-  @Field(() => Int, {description: ''})
+  @Field(() => Int, { description: 'The value to be compared against' })
   value: number;
 }
 
 @InputType()
 export class QueryPlayerStatsInput {
   @Field(() => [Int], {
-    description: 'Example field (placeholder)',
+    description: 'An array of games to query',
     nullable: true,
   })
   gameIds?: number[];
 
-  @Field(() => [Int], { description: '', nullable: true })
+  @Field(() => [Int], {
+    description: 'An array of players to query',
+    nullable: true,
+  })
   playerIds?: number[];
 
-  @Field(() => [String], { description: '', nullable: true })
+  @Field(() => [String], {
+    description: 'An array of player numbers to query',
+    nullable: true,
+  })
   playerNumbers?: string[];
 
   @Field(() => QueryPlayerStatsQueryOptionsInput, {
-    description: '',
     nullable: true,
   })
   options?: QueryPlayerStatsQueryOptionsInput;
 
-  @Field(() => QueryPlayerStatsStatisticsInput, { description: '', nullable: true })
+  @Field(() => QueryPlayerStatsStatisticsInput, {
+    description: 'Player age',
+    nullable: true,
+  })
   playerAge: QueryPlayerStatsStatisticsInput;
 
   @Field(() => QueryPlayerStatsStatisticsInput, {
-    description: '',
+    description: 'Player assists',
     nullable: true,
   })
   assists: QueryPlayerStatsStatisticsInput;
 
-  @Field(() => QueryPlayerStatsStatisticsInput, { description: '', nullable: true })
+  @Field(() => QueryPlayerStatsStatisticsInput, {
+    description: 'Player goals',
+    nullable: true,
+  })
   goals: QueryPlayerStatsStatisticsInput;
 
-  @Field(() => QueryPlayerStatsStatisticsInput, { description: '', nullable: true })
+  @Field(() => QueryPlayerStatsStatisticsInput, {
+    description: 'Player hits',
+    nullable: true,
+  })
   hits: QueryPlayerStatsStatisticsInput;
 
-  @Field(() => QueryPlayerStatsStatisticsInput, { description: '', nullable: true })
+  @Field(() => QueryPlayerStatsStatisticsInput, {
+    description: 'Player points',
+    nullable: true,
+  })
   points: QueryPlayerStatsStatisticsInput;
 }
--- api/src/player-stats/entities/player-stats.entity.ts
+++ api/src/player-stats/entities/player-stats.entity.ts
@@ -5,45 +5,45 @@ export class PlayerStats {
   @Field(() => Int, { description: 'Id of the game' })
   gameId: number;
 
-  @Field(() => Int, { description: '' })
+  @Field(() => Int, { description: 'Id of the player' })
   playerId: number;
 
-  @Field(() => String, { description: '' })
+  @Field(() => String, { description: 'Player full name' })
   playerName: string;
 
-  @Field(() => Int, { description: '' })
+  @Field(() => Int, { description: 'Id of the team' })
   teamId: number;
 
-  @Field(() => String, { description: '' })
+  @Field(() => String, { description: 'Team name' })
   teamName: string;
 
-  @Field(() => Int, { description: '', nullable: true })
+  @Field(() => Int, { description: 'Player age', nullable: true })
   playerAge?: number;
 
-  @Field(() => String, { description: '', nullable: true })
+  @Field(() => String, { description: 'Player number', nullable: true })
   playerNumber?: string;
 
-  @Field(() => String, { description: '' })
+  @Field(() => String, { description: 'Player position' })
   playerPosition: string;
 
-  @Field(() => Int, { description: '' })
+  @Field(() => Int, { description: 'Amount of assists' })
   assists: number;
 
-  @Field(() => Int, { description: '' })
+  @Field(() => Int, { description: 'Amount of goals' })
   goals: number;
 
-  @Field(() => Int, { description: '' })
+  @Field(() => Int, { description: 'Amount of hits' })
   hits: number;
 
-  @Field(() => Int, { description: '' })
+  @Field(() => Int, { description: 'Amount of points' })
   points: number;
 
-  @Field(() => Int, { description: '' })
+  @Field(() => Int, { description: 'Penalty minutes' })
   penaltyMinutes: number;
 
-  @Field(() => String, { description: '' })
+  @Field(() => String, { description: 'Opponent team name' })
   opponentTeamName: string;
 
-  @Field(() => Int, { description: '' })
+  @Field(() => Int, { description: 'Id of opponent team' })
   opponentTeamId: number;
 }
