820544a5c96cf485144569f662777ce30c7e5709
Jelani Jackson <jelani.t.jackson@gmail.com>
2023-06-05 17:40:02 -0500

    Injecting axios as httpService which uses observables

--- etl/src/nhl/nhl.service.ts
+++ etl/src/nhl/nhl.service.ts
@@ -1,23 +1,19 @@
 import { HttpException, Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
 import { SeasonDto, GameDto } from './dto';
 import axiosRetry from 'axios-retry';
+import { HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class NhlService {
-  private readonly axios: AxiosInstance;
-
-  constructor() {
-    this.axios = axios.create({
-      baseURL: 'https://statsapi.web.nhl.com/api/v1',
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    axiosRetry(this.axios, { retries: 3 });
+  constructor(private readonly httpService: HttpService) {
+    axiosRetry(httpService.axiosRef, { retries: 3 });
   }
 
   async getSeason(seasonId: string): Promise<SeasonDto> {
-    const seasonResponse = await this.axios.get(`/schedule?season=${seasonId}`);
+    const seasonResponse = await lastValueFrom(
+      this.httpService.get(`/schedule?season=${seasonId}`),
+    );
 
     if (seasonResponse.status >= 400) {
       throw new HttpException(seasonResponse.statusText, seasonResponse.status);
@@ -27,7 +23,9 @@ export class NhlService {
   }
 
   async getGame(gameId: number): Promise<GameDto> {
-    const gameResponse = await this.axios.get(`/game/${gameId}/feed/live`);
+    const gameResponse = await lastValueFrom(
+      this.httpService.get(`/game/${gameId}/feed/live`),
+    );
 
     if (gameResponse.status >= 400) {
       throw new HttpException(gameResponse.statusText, gameResponse.status);
