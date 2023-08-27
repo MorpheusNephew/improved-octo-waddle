32ea3abf775aac0dc1253c040a0e8442dde39617
Jelani Jackson <jelani.t.jackson@gmail.com>
2023-08-27 08:43:43 +0300

    Moved private function below public

--- db-service/internal/database/utils.go
+++ db-service/internal/database/utils.go
@@ -10,18 +10,6 @@ import (
 	"gorm.io/gorm"
 )
 
-func getDatabaseDsn() string {
-	godotenv.Load()
-
-	host := os.Getenv("POSTGRES_HOST")
-	user := os.Getenv("POSTGRES_USER")
-	password := os.Getenv("POSTGRES_PASSWORD")
-	dbname := os.Getenv("POSTGRES_DB")
-	port := os.Getenv("POSTGRES_PORT")
-
-	return fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbname, port)
-}
-
 func GetDatabase() *gorm.DB {
 	dsn := getDatabaseDsn()
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
@@ -34,4 +22,16 @@ func GetDatabase() *gorm.DB {
 	db.AutoMigrate(&models.PlayerGameStats{})
 
 	return db
+}
+
+func getDatabaseDsn() string {
+	godotenv.Load()
+
+	host := os.Getenv("POSTGRES_HOST")
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	dbName := os.Getenv("POSTGRES_DB")
+	port := os.Getenv("POSTGRES_PORT")
+
+	return fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbName, port)
 }
