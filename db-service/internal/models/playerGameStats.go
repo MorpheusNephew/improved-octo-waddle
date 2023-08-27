6ec56fc279e59b76395f760fcd9055189c83c2e0
Jelani Jackson <jelani.t.jackson@gmail.com>
2023-08-27 08:43:27 +0300

    Included json tags

--- db-service/internal/models/playerGameStats.go
+++ db-service/internal/models/playerGameStats.go
@@ -3,23 +3,23 @@ package models
 import "time"
 
 type PlayerGameStats struct {
-	GameId           int       `gorm:"primary_key;column:gameId"`
-	PlayerId         int       `gorm:"primary_key:column:playerId"`
-	PlayerName       string    `gorm:"type:varchar(255);column:playerName"`
-	TeamId           int       `gorm:"column:teamId"`
-	TeamName         string    `gorm:"type:varchar(255);column:teamName"`
-	PlayerAge        int       `gorm:"column:playerAge"`
-	PlayerNumber     string    `gorm:"type:varchar(255);column:playerNumber"`
-	PlayerPosition   string    `gorm:"type:varchar(255);column:playerPosition"`
-	Assists          int       `gorm:"column:assists"`
-	Goals            int       `gorm:"column:goals"`
-	Hits             int       `gorm:"column:hits"`
-	Points           int       `gorm:"column:points"`
-	PenaltyMinutes   int       `gorm:"column:penaltyMinutes"`
-	OpponentTeamName string    `gorm:"type:varchar(255);column:opponentTeamName"`
-	OpponentTeamId   int       `gorm:"column:opponentTeamId"`
-	CreatedAt        time.Time `gorm:"column:createdAt"`
-	UpdatedAt        time.Time `gorm:"column:updatedAt"`
+	GameId           int       `gorm:"primary_key;column:gameId" json:"gameId"`
+	PlayerId         int       `gorm:"primary_key:column:playerId" json:"playerId"`
+	PlayerName       string    `gorm:"type:varchar(255);column:playerName" json:"playerName"`
+	TeamId           int       `gorm:"column:teamId" json:"teamId"`
+	TeamName         string    `gorm:"type:varchar(255);column:teamName" json:"teamName"`
+	PlayerAge        int       `gorm:"column:playerAge" json:"playerAge"`
+	PlayerNumber     string    `gorm:"type:varchar(255);column:playerNumber" json:"playerNumber"`
+	PlayerPosition   string    `gorm:"type:varchar(255);column:playerPosition" json:"playerPosition"`
+	Assists          int       `gorm:"column:assists" json:"assists"`
+	Goals            int       `gorm:"column:goals" json:"goals"`
+	Hits             int       `gorm:"column:hits" json:"hits"`
+	Points           int       `gorm:"column:points" json:"points"`
+	PenaltyMinutes   int       `gorm:"column:penaltyMinutes" json:"penaltyMinutes"`
+	OpponentTeamName string    `gorm:"type:varchar(255);column:opponentTeamName" json:"opponentTeamName"`
+	OpponentTeamId   int       `gorm:"column:opponentTeamId" json:"opponentTeamId"`
+	CreatedAt        time.Time `gorm:"column:createdAt;autoCreateTime" json:"-"`
+	UpdatedAt        time.Time `gorm:"column:updatedAt;autoUpdateTime" json:"-"`
 }
 
 func (PlayerGameStats) TableName() string {
