9fd8fbd6076a0f592213993114c8b0a28d32d640
Jelani Jackson <jelani.t.jackson@gmail.com>
2023-08-27 07:56:21 +0300

    Implemented runner function

--- db-service/cmd/main.go
+++ db-service/cmd/main.go
@@ -2,44 +2,17 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
 
-	"github.com/morpheusnephew/db-service/v2/internal/database"
 	"github.com/morpheusnephew/db-service/v2/internal/models"
+	"github.com/morpheusnephew/db-service/v2/internal/utils"
+	"gorm.io/gorm"
 )
 
 func main() {
-	fmt.Println("Connecting to database")
+	utils.Runner(func(db *gorm.DB) {
+		var results []models.PlayerGameStats
+		db.Where("\"gameId\" = ?", 2018020162).Find(&results)
 
-	db := database.GetDatabase()
-
-	fmt.Println("Connected to postgres db")
-
-	done := getDoneChannel()
-
-	var results []models.PlayerGameStats
-	db.Where("\"gameId\" = ?", 2018020162).Find(&results)
-
-	fmt.Println(results)
-
-	<-done
-	fmt.Println("Exiting")
-}
-
-func getDoneChannel() chan bool {
-	sigs := make(chan os.Signal, 1)
-	done := make(chan bool, 1)
-
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-
-	go func() {
-		sig := <-sigs
-		fmt.Println()
-		fmt.Println(sig)
-		done <- true
-	}()
-
-	return done
+		fmt.Println(results)
+	})
 }
