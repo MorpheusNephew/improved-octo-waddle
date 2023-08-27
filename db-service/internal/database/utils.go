1216837cc448250b7eb16489387027399dc59aff
Jelani Jackson <jelani.t.jackson@gmail.com>
2023-08-27 07:26:30 +0300

    Using environment variables to connect to db

--- db-service/internal/database/utils.go
+++ db-service/internal/database/utils.go
@@ -4,13 +4,26 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/morpheusnephew/db-service/v2/internal/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
+func getDatabaseDsn() string {
+	godotenv.Load()
+
+	host := os.Getenv("POSTGRES_HOST")
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	dbname := os.Getenv("POSTGRES_DB")
+	port := os.Getenv("POSTGRES_PORT")
+
+	return fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbname, port)
+}
+
 func GetDatabase() *gorm.DB {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", "localhost", "postgres", "postgres", "sports", "5432")
+	dsn := getDatabaseDsn()
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
